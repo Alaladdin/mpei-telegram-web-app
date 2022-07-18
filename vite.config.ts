5580a4456345e69533ef96df6b3509daabc335e6
Aladdin <g.tabroid@gmail.com>
2022-07-19 01:22:27 +0300

    v 0.0.1

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,16 @@ import vue from '@vitejs/plugin-vue';
 import { defineConfig, loadEnv } from 'vite';
 import WindiCSS from 'vite-plugin-windicss';
 
+// eslint-disable-next-line max-lines-per-function
 export default ({ mode }: { mode:string }) => {
     const env = loadEnv(mode, process.cwd());
+    const isDev = mode === 'development';
 
     return defineConfig({
         plugins: [vue(), WindiCSS()],
         resolve: {
-            alias: {
+            extensions: ['.ts', '.js', '.json'],
+            alias     : {
                 '@': fileURLToPath(new URL('./src', import.meta.url)),
             },
         },
@@ -17,7 +20,8 @@ export default ({ mode }: { mode:string }) => {
             proxy: {
                 '/api': {
                     target      : env.VITE_API_ADDRESS,
-                    changeOrigin: true,
+                    changeOrigin: isDev,
+                    secure      : !isDev,
                     headers     : {
                         AuthToken: env.VITE_AUTH_TOKEN || '',
                     },
